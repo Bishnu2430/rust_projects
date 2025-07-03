6e782de852aca0a7fc5077dab5d7f806196a0e8a
Bishnu2430 <karbishnu2430@gmail.com>
2025-07-03 21:34:26 +0530

    Updated the file structure

---
+++ Chapter 3/variables/target/debug/deps/variables-04d50ce308428d9f.d
@@ -0,0 +1,5 @@
+/mnt/d/rust_projects/variables/target/debug/deps/variables-04d50ce308428d9f: src/main.rs
+
+/mnt/d/rust_projects/variables/target/debug/deps/variables-04d50ce308428d9f.d: src/main.rs
+
+src/main.rs:
---
+++ Chapter 3/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/mnt/d/rust_projects/variables/target/debug/variables: /mnt/d/rust_projects/variables/src/main.rs
